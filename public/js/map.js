3a1ae56f52a21b5c01fcedf42268dcf4e8bda887
Ochikun <earlybread3044@gmail.com>
2026-03-12 01:38:27 +0900

    Change:nginx php-fpmに変更

--- public/js/map.js
+++ public/js/map.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // map初期化 表示
     const map = L.map('map');
     L.tileLayer('https://tile.openstreetmap.jp/{z}/{x}/{y}.png', {
-        attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+        attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors,"
     }).addTo(map);
 
     //住所検索機能
